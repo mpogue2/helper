94fe6fbbb7e8d99cc780fdb3804cddb4d560e3fe
Mike Pogue <mpogue@zenstarstudio.com>
2022-02-13 16:40:56 -0800

    Change to V1.7

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,7 +12,7 @@
         />
 
         <q-toolbar-title>
-          SquareDesk Tip Planner V1.6
+          SquareDesk Tip Planner V1.7
         </q-toolbar-title>
 
         <q-tabs v-model="whichTab" shrink stretch dense inline-label>
